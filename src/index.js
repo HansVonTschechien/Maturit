0350ca821a95547018ff7f042e1d5644cb64b69f
Jan Honc <jan.honc@student.spsmb.cz>
2023-09-21 23:22:13 +0200

    oprava chyby při spouštění

--- src/index.js
+++ src/index.js
@@ -18,7 +18,6 @@ const createWindow = () => {
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
-  mainWindow.loadFile(path.join(__dirname, 'cj.html'));
   
 
   // Open the DevTools.
